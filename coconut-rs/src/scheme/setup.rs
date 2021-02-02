70444042696a45689b9d1338137ebebf50e9fda4
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-02-02 15:08:28 +0000

    Placeholder setup

---
+++ coconut-rs/src/scheme/setup.rs
@@ -0,0 +1,75 @@
+// Copyright 2021 Nym Technologies SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::error::Result;
+use bls12_381::{G1Affine, G2Affine, Scalar};
+use rand_core::{RngCore, CryptoRng};
+use ff::Field;
+
+pub struct Parameters<R>
+    where
+        R: RngCore + CryptoRng,
+{
+    // .... or Projective?
+    g1: G1Affine,
+    hs: Vec<G1Affine>,
+    g2: G2Affine,
+    rng: R,
+}
+
+impl<R> Parameters<R>
+    where
+        R: RngCore + CryptoRng,
+{
+    pub fn new(rng: R, num_attributes: u32) -> Parameters<R> {
+
+        // requires hash to point
+        let hs = todo!();
+
+
+        Parameters {
+            g1: G1Affine::generator(),
+            hs,
+            g2: G2Affine::generator(),
+            rng,
+        }
+    }
+
+    pub(crate) fn gen1(&self) -> &G1Affine {
+        &self.g1
+    }
+
+    pub(crate) fn gen2(&self) -> &G2Affine {
+        &self.g2
+    }
+
+    pub(crate) fn random_scalar(&mut self) -> Scalar {
+        Scalar::random(&mut self.rng)
+    }
+}
+
+// for ease of use in tests requiring params
+// TODO: not sure if this will have to go away when tests require some specific number of generators
+#[cfg(test)]
+impl Default for Parameters<rand_core::OsRng> {
+    fn default() -> Self {
+        Parameters {
+            g1: G1Affine::generator(),
+            hs: Vec::new(),
+            g2: G2Affine::generator(),
+            rng: rand_core::OsRng,
+        }
+    }
+}
+
