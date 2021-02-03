90e1f009d42543ebfd2c1ec258484cccb608c675
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-02-03 16:42:46 +0000

    Generalised lagrangian interpolation

--- coconut-rs/src/utils.rs
+++ coconut-rs/src/utils.rs
@@ -16,51 +16,74 @@
 // use digest::Update;
 // use generic_array::{ArrayLength, GenericArray};
 
+use std::any::Any;
+use std::iter::Sum;
+use std::ops::Mul;
+
+use bls12_381::{G2Projective, Scalar};
+
 use crate::error::Result;
-use bls12_381::Scalar;
 
 /// Evaluates a polynomial defined by the slice of coefficients at point x.
-pub fn evaluate_polynomial(coefficients: &[&Scalar], x: &Scalar) -> Scalar {
+pub(crate) fn evaluate_polynomial(coefficients: &[Scalar], x: &Scalar) -> Scalar {
     coefficients
         .iter()
         .enumerate()
-        .map(|(i, &coefficient)| coefficient * x.pow(&[i as u64, 0, 0, 0])) // coefficient[n] * x ^ n
+        .map(|(i, coefficient)| coefficient * x.pow(&[i as u64, 0, 0, 0])) // coefficient[n] * x ^ n
         .sum()
 }
 
-
-fn generate_lagrangian_coefficients_at_origin(points: &[u64]) -> Vec<Scalar> {
+#[inline]
+pub(crate) fn generate_lagrangian_coefficients_at_origin(points: &[u64]) -> Vec<Scalar> {
     let x = Scalar::zero();
 
-    points.iter().enumerate().map(|(i, point_i)| {
-        let mut numerator = Scalar::one();
-        let mut denominator = Scalar::one();
-        let xi = Scalar::from(*point_i);
+    points
+        .iter()
+        .enumerate()
+        .map(|(i, point_i)| {
+            let mut numerator = Scalar::one();
+            let mut denominator = Scalar::one();
+            let xi = Scalar::from(*point_i);
 
-        for (j, point_j) in points.iter().enumerate() {
-            if j != i {
-                let xj = Scalar::from(*point_j);
+            for (j, point_j) in points.iter().enumerate() {
+                if j != i {
+                    let xj = Scalar::from(*point_j);
 
-                // numerator = (x - xs[0]) * ... * (x - xs[j]), j != i
-                numerator *= x - xj;
+                    // numerator = (x - xs[0]) * ... * (x - xs[j]), j != i
+                    numerator *= x - xj;
 
-                // denominator = (xs[i] - x[0]) * ... * (xs[i] - x[j]), j != i
-                denominator *= xi - xj;
+                    // denominator = (xs[i] - x[0]) * ... * (xs[i] - x[j]), j != i
+                    denominator *= xi - xj;
+                }
             }
-        }
-        // numerator / denominator
-        numerator * denominator.invert().unwrap()
-    }).collect()
+            // numerator / denominator
+            numerator * denominator.invert().unwrap()
+        })
+        .collect()
 }
 
 /// Performs a Lagrange interpolation at the origin for a polynomial defined by `points` and `values`.
-pub fn perform_lagrangian_interpolation_at_origin(points: &[u64], values: &[&Scalar]) -> Result<Scalar> {
+/// It can be used for Scalars, G1 and G2 points.
+pub(crate) fn perform_lagrangian_interpolation_at_origin<T>(
+    points: &[u64],
+    values: &[T],
+) -> Result<T>
+where
+    T: Sum,
+    // first time I've had to use it, for reference it's called higher ranked trait bounds:
+    // https://github.com/rust-lang/rfcs/blob/master/text/0387-higher-ranked-trait-bounds.md
+    for<'a> &'a T: Mul<Scalar, Output = T>,
+{
     if points.len() != values.len() {
         // return Err
     }
 
     let coefficients = generate_lagrangian_coefficients_at_origin(points);
-    Ok(coefficients.into_iter().zip(values.iter()).map(|(coeff, &val)| coeff * val).sum())
+    Ok(coefficients
+        .into_iter()
+        .zip(values.iter())
+        .map(|(coeff, val)| val * coeff)
+        .sum())
 }
 
 // pub trait PointHash {
@@ -85,37 +108,58 @@ mod tests {
     #[test]
     fn polynomial_evaluation() {
         // y = 42 (it should be 42 regardless of x)
-        let coeff = &[&Scalar::from(42)];
+        let coeff = &[Scalar::from(42)];
 
-        assert_eq!(Scalar::from(42), evaluate_polynomial(coeff, &Scalar::from(1)));
-        assert_eq!(Scalar::from(42), evaluate_polynomial(coeff, &Scalar::from(0)));
-        assert_eq!(Scalar::from(42), evaluate_polynomial(coeff, &Scalar::from(10)));
+        assert_eq!(
+            Scalar::from(42),
+            evaluate_polynomial(coeff, &Scalar::from(1))
+        );
+        assert_eq!(
+            Scalar::from(42),
+            evaluate_polynomial(coeff, &Scalar::from(0))
+        );
+        assert_eq!(
+            Scalar::from(42),
+            evaluate_polynomial(coeff, &Scalar::from(10))
+        );
 
         // y = x + 10, at x = 2 (exp: 12)
-        let coeff = &[&Scalar::from(10), &Scalar::from(1)];
+        let coeff = &[Scalar::from(10), Scalar::from(1)];
 
-        assert_eq!(Scalar::from(12), evaluate_polynomial(coeff, &Scalar::from(2)));
+        assert_eq!(
+            Scalar::from(12),
+            evaluate_polynomial(coeff, &Scalar::from(2))
+        );
 
         // y = x^4 - 5x^2 + 2x - 3, at x = 3 (exp: 39)
-        let coeff = &[&(-Scalar::from(3)), &Scalar::from(2), &(-Scalar::from(5)), &Scalar::zero(), &Scalar::from(1)];
+        let coeff = &[
+            (-Scalar::from(3)),
+            Scalar::from(2),
+            (-Scalar::from(5)),
+            Scalar::zero(),
+            Scalar::from(1),
+        ];
 
-        assert_eq!(Scalar::from(39), evaluate_polynomial(coeff, &Scalar::from(3)));
+        assert_eq!(
+            Scalar::from(39),
+            evaluate_polynomial(coeff, &Scalar::from(3))
+        );
     }
 
     #[test]
-    fn performing_lagrangian_interpolation_at_origin() {
+    fn performing_lagrangian_scalar_interpolation_at_origin() {
         // x^2 + 3
         // x, f(x):
         // 1, 4,
         // 2, 7,
         // 3, 12,
         let points = vec![1, 2, 3];
-        let y1 = &Scalar::from(4);
-        let y2 = &Scalar::from(7);
-        let y3 = &Scalar::from(12);
-        let values = vec![y1, y2, y3];
+        let values = vec![Scalar::from(4), Scalar::from(7), Scalar::from(12)];
 
-        assert_eq!(Scalar::from(3), perform_lagrangian_interpolation_at_origin(&points, &values).unwrap());
+        assert_eq!(
+            Scalar::from(3),
+            perform_lagrangian_interpolation_at_origin(&points, &values).unwrap()
+        );
 
         // x^3 + 3x^2 - 5x + 11
         // x, f(x):
@@ -124,12 +168,16 @@ mod tests {
         // 3, 50
         // 4, 103
         let points = vec![1, 2, 3, 4];
-        let y1 = &Scalar::from(10);
-        let y2 = &Scalar::from(21);
-        let y3 = &Scalar::from(50);
-        let y4 = &Scalar::from(103);
-        let values = vec![y1, y2, y3, y4];
+        let values = vec![
+            Scalar::from(10),
+            Scalar::from(21),
+            Scalar::from(50),
+            Scalar::from(103),
+        ];
 
-        assert_eq!(Scalar::from(11), perform_lagrangian_interpolation_at_origin(&points, &values).unwrap());
+        assert_eq!(
+            Scalar::from(11),
+            perform_lagrangian_interpolation_at_origin(&points, &values).unwrap()
+        );
     }
 }
